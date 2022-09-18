0fc43d5f83d8da98f81128fc336c9cc2a3049734
Mouayad <84500993+Mouayad91@users.noreply.github.com>
2022-09-19 01:06:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Arr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String[] food = new string[4];
+
+            food[0] = "Meat";
+            food[1] = "Rice";
+            food[2] = "Chicken";
+            food[3] = "Bread";
+
+            //String food [] = {"Meat" , "Rice" , "Chicken" , "Bread"};
+
+            for(int i = 0; i < food.Length; i++)
+            {
+
+                Console.WriteLine(food[i]);
+
+            }
+
+         
+
+
+
+        }
+    }
+}
